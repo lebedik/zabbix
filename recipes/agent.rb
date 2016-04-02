b249dd78c4423feef2f1c27b99e96b23129e9663
AlexBar <barhanow@gmail.com>
2016-04-02 15:10:10 +0300

    api add host

--- recipes/agent.rb
+++ recipes/agent.rb
@@ -2,6 +2,37 @@ if node['platform'] == 'centos'
 
 hostname = node['fqdn']
 
+# Install zabbixapi gem
+execute 'zabbix_api_install' do
+        command '/opt/chef/embedded/bin/gem install zabbixapi'
+end
+
+# Create host on zabbix server
+ruby "create_host" do
+code <<-EOH
+require "zabbixapi"
+        zbx = ZabbixApi.connect(
+  :url => "http://#{node['zabbix']['zabbixServerAddress']}/api_jsonrpc.php",
+  :user => 'Admin',
+  :password => 'zabbix'
+)
+zbx.hosts.create_or_update(
+  :host => "#{node['fqdn']}",
+  :interfaces => [
+    {
+      :type => 1,
+      :main => 1,
+      :ip => "#{node['ipaddress']}",
+      :dns => "#{node['fqdn']}",
+      :port => 10050,
+      :useip => 0
+    }
+  ],
+  :groups => [ :groupid => zbx.hostgroups.get_id(:name => "db-test") ]
+)
+EOH
+end
+
 
   if node['platform_version'].to_i >= 7
       zabbix_release = "#{Chef::Config[:file_cache_path]}/zabbix-release-3.0-1.el7.noarch.rpm"
@@ -34,7 +65,7 @@ hostname = node['fqdn']
   end
 
   service "zabbix-agent" do
-    action :nothing
+    action [:enable :nothing]
   end
 
   template '/etc/zabbix/zabbix_agentd.conf' do
