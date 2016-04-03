ce72fb9fd7703feb8502c25fb2232f7d85788851
AlexBar <barhanow@gmail.com>
2016-04-03 17:46:00 +0300

    f

--- recipes/agent.rb
+++ recipes/agent.rb
@@ -236,7 +236,7 @@ end
       notifies :restart, 'service[zabbix-agent]', :delayed
     end
 
-    cookbook_file '/etc/zabbix/zabbix_agent.d/mysql_status.conf' do
+    cookbook_file '/etc/zabbix/zabbix_agentd.d/mysql_status.conf' do
       source 'mysql_status.conf'
       owner 'zabbix'
       group 'zabbix'
